0fb8dbe4678ac1802f23365b39daa07309312cf0
Umut Bayri <bayriumut8016@gmail.com>
2021-10-22 00:28:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,43 @@
+# -*- coding : utf-8 -*- 
+
 import requests
 from bs4 import BeautifulSoup
 
 import smtplib as sm
 import datetime
 
-now = datetime.datetime.now()
-date = now.strftime("%Y %m %d")
-print()
-filew = open(f"news\\abc.txt", "w")
-
 URL = "https://news.ycombinator.com/newest"
 user_agent = {
-    "User-Agent" : "User-Agent'in"
+    "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+ AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36"
 }
+
+now = datetime.datetime.now()
+date = now.strftime("%Y %m %d")
+filew = open(f"klasörler\\{date}.txt", "w")
 
 page = requests.get(URL, headers = user_agent)
 page_content = BeautifulSoup(page.text, "html.parser")
 titles = page_content.find_all(rel = "nofollow", limit = 4)
 
 for title in titles:
-    template = f"""\
+    template = f"""\n\
     TITLE : {title.get_text()}
     LINK : {title.get("href")}
     """
-
-    print(template, file = filew)
+    print(template, file = filew, flush = True)
 
 filew.close()
-filer = open(f"news/{date}")
+filer = open(f"klasörler/{date}.txt")
+mail_content = filer.read()
 
-gonderen_kullanici = "MailAdresin@gmail.com"
-gonderen_sifre = 'Sifren'
-alici_mail= 'Alici@gmail.com'
+gonderen_kullanici = "x@gmail.com"
+gonderen_sifre = 'x'
+alici_mail= 'x@gmail.com'
 
 server = sm.SMTP('smtp.gmail.com:587') 
 server.starttls()  
 server.login(gonderen_kullanici , gonderen_sifre) 
 
-server.sendmail(gonderen_kullanici, alici_mail, )
+server.sendmail(gonderen_kullanici, alici_mail, mail_content)
 server.close() 
